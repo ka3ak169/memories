1d2018ce4095d6e3b4366a49a7610cfbbeeba621
Aleksey Kazakov <ka3akne@mail.ru>
2022-12-05 22:00:04 +0400

    реализовать открытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,10 @@ const menu = document.querySelector('.header');
 const langBox = menu.querySelector('.header__lang-box');
 const openBtn = document.querySelector('.presentation__open-menu');
 const closeBtn = langBox.querySelector('.header__close-btn');
+const workPopup = document.querySelector('.work-popup');
+const openWorkPopBtn = document.querySelector('.portfolio__add-btn');
+const closeWorkPopBtn = workPopup.querySelector('.work-popup__close-btn');
+
 
 
 const openMenu = function() {
@@ -14,5 +18,24 @@ const closeMenu = function() {
   langBox.classList.remove('header__lang-box_open');
 }
 
+const openWorkPopup = function() {
+  workPopup.classList.add('work-popup_open');
+}
+
+const closeWorkpopup = function() {
+  workPopup.classList.remove('work-popup_open');
+}
+
+const closeWorkByOverlay = function(evt) {
+  console.log(evt.target, evt.currentTarget);
+  if(evt.target !== evt.currentTarget) {
+    return
+  };
+  closeWorkpopup();
+}
+
 openBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
+openWorkPopBtn.addEventListener('click', openWorkPopup);
+closeWorkPopBtn.addEventListener('click', closeWorkpopup);
+workPopup.addEventListener('click', closeWorkByOverlay)
