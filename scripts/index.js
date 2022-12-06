d44c483f62667271baf61d6a5ace89232b2fe791
Aleksey Kazakov <ka3akne@mail.ru>
2022-12-07 03:43:16 +0400

    реализовать добавление работ

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,27 @@
 const selectors = {
-  menu: '.header',
-  langBox: '.header__lang-box',
-  openBtn: '.presentation__open-menu',
-  closeBtn: '.header__close-btn',
-  workPopup: '.work-popup',
-  openWorkPopBtn: '.portfolio__add-btn',
-  closeWorkPopBtn: '.work-popup__close-btn',
-  workForm: '.work-popup__form',
-  workInputName: '.work-popup__input_name',
-  workInputImage: '.work-popup__input_image',
-  portfolioTitle: '.portfolio__title',
-  skillsTemplate: '.skills-template',
-  skillsPhoto: '.skills__photo',
-  skillsName: '.skills__program-name',
-  skillsContainer: '.skills__program',
-  skillsSection: '.skills__programs',
-  
-}
+  menu: ".header",
+  langBox: ".header__lang-box",
+  openBtn: ".presentation__open-menu",
+  closeBtn: ".header__close-btn",
+  workPopup: ".work-popup",
+  openWorkPopBtn: ".portfolio__add-btn",
+  closeWorkPopBtn: ".work-popup__close-btn",
+  workForm: ".work-popup__form",
+  workInputName: ".work-popup__input_name",
+  workInputImage: ".work-popup__input_image",
+  portfolioTitle: ".portfolio__title",
+  skillsTemplate: ".skills-template",
+  skillsPhoto: ".skills__photo",
+  skillsName: ".skills__program-name",
+  skillsContainer: ".skills__program",
+  skillsSection: ".skills__programs",
+  portfolioTemplate: ".portfolio-template",
+  portfolioCard: ".portfolio__card",
+  portfolioContainer: ".portfolio__cards",
+  portfolioCloseBtn: ".portfolio__close-btn",
+  portfoiloImage: ".portfolio__image",
+  portfolioName: ".portfolio__sign",
+};
 
 const menu = document.querySelector(selectors.menu);
 const langBox = menu.querySelector(selectors.langBox);
@@ -30,41 +35,37 @@ const workInputName = workForm.querySelector(selectors.workInputName);
 const workInputImage = workForm.querySelector(selectors.workInputImage);
 const portfolioTitle = document.querySelector(selectors.portfolioTitle);
 
-
-
 function openMenu() {
-  menu.classList.add('header_open');
-  langBox.classList.add('header__lang-box_open');
+  menu.classList.add("header_open");
+  langBox.classList.add("header__lang-box_open");
 }
 
 function closeMenu() {
-  menu.classList.remove('header_open');
-  langBox.classList.remove('header__lang-box_open');
+  menu.classList.remove("header_open");
+  langBox.classList.remove("header__lang-box_open");
 }
 
 function openWorkPopup() {
-  workInputName.value = portfolioTitle.textContent;
-  workPopup.classList.add('work-popup_open');
+  workPopup.classList.add("work-popup_open");
 }
 
 function closeWorkpopup() {
-  workPopup.classList.remove('work-popup_open');
+  workPopup.classList.remove("work-popup_open");
+  workForm.reset();
 }
 
 function closeWorkByOverlay(evt) {
-  if(evt.target !== evt.currentTarget) {
-    return
-  };
+  if (evt.target !== evt.currentTarget) {
+    return;
+  }
   closeWorkpopup();
 }
 
-function handleFormSbm(evt) {
-  evt.preventDefault();
-  portfolioTitle.textContent = workInputName.value;
-}
-
 function createSkillsCard(name, image) {
-  const template = document.querySelector(selectors.skillsTemplate).content.querySelector(selectors.skillsContainer).cloneNode(true);
+  const template = document
+    .querySelector(selectors.skillsTemplate)
+    .content.querySelector(selectors.skillsContainer)
+    .cloneNode(true);
   const skillsSection = document.querySelector(selectors.skillsSection);
   const skillsPhoto = template.querySelector(selectors.skillsPhoto);
   const skillsName = template.querySelector(selectors.skillsName);
@@ -79,34 +80,91 @@ function createSkillsCard(name, image) {
 function createInitialSkillsCard() {
   const initialSkills = [
     {
-      name: 'Adobe Illustrator',
-      image: './images/AI.png',
+      name: "Adobe Illustrator",
+      image: "./images/AI.png",
     },
     {
-      name: 'Adobe After Effects',
-      image: './images/AE.png',
+      name: "Adobe After Effects",
+      image: "./images/AE.png",
     },
     {
-      name: 'Adobe Photoshop',
-      image: './images/PS.png',
+      name: "Adobe Photoshop",
+      image: "./images/PS.png",
     },
     {
-      name: 'Figma',
-      image: './images/Figma.png',
+      name: "Figma",
+      image: "./images/Figma.png",
     },
   ];
 
-  initialSkills.forEach(function(card) {
+  initialSkills.forEach(function (card) {
     createSkillsCard(card.name, card.image);
   });
 }
 
 createInitialSkillsCard();
 
+function createPortfolioCard(name, link) {
+  const portfolioTemplate = document
+    .querySelector(selectors.portfolioTemplate)
+    .content.querySelector(selectors.portfolioCard)
+    .cloneNode(true);
+  const portfolioContainer = document.querySelector(
+    selectors.portfolioContainer
+  );
+  const portfoiloImage = portfolioTemplate.querySelector(
+    selectors.portfoiloImage
+  );
+  const portfolioName = portfolioTemplate.querySelector(
+    selectors.portfolioName
+  );
+  const portfolioCloseBtn = portfolioTemplate.querySelector(
+    selectors.portfolioCloseBtn
+  );
 
-openBtn.addEventListener('click', openMenu);
-closeBtn.addEventListener('click', closeMenu);
-openWorkPopBtn.addEventListener('click', openWorkPopup);
-closeWorkPopBtn.addEventListener('click', closeWorkpopup);
-workPopup.addEventListener('click', closeWorkByOverlay);
-workForm.addEventListener('submit', handleFormSbm);
+  portfoiloImage.src = link;
+  portfoiloImage.atl = name;
+  portfolioName.textContent = name;
+
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioTemplate.remove();
+  });
+
+  portfolioContainer.appendChild(portfolioTemplate);
+}
+
+function createInitialPortCard() {
+  const initialPortfolio = [
+    {
+      name: "Online fashion store - Homepage",
+      link: "./images/fashion.png",
+    },
+    {
+      name: "Reebok Store - Concept",
+      link: "./images/reebok_web.png",
+    },
+    {
+      name: "Braun Landing Page - Concept",
+      link: "./images/braun.png",
+    },
+  ];
+
+  initialPortfolio.forEach((card) => {
+    createPortfolioCard(card.name, card.link);
+  });
+}
+
+createInitialPortCard();
+
+function popupFormSbm(evt) {
+  evt.preventDefault();
+  createPortfolioCard(workInputName.value, workInputImage.value);
+  closeWorkpopup();
+}
+
+openBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+openWorkPopBtn.addEventListener("click", openWorkPopup);
+closeWorkPopBtn.addEventListener("click", closeWorkpopup);
+workPopup.addEventListener("click", closeWorkByOverlay);
+workForm.addEventListener("submit", popupFormSbm);
