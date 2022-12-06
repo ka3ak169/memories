1268b992438269340164184452854662b20e9b1e
Aleksey Kazakov <ka3akne@mail.ru>
2022-12-07 00:02:51 +0400

    реализовать добавление программ

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,112 @@
-const menu = document.querySelector('.header');
-const langBox = menu.querySelector('.header__lang-box');
-const openBtn = document.querySelector('.presentation__open-menu');
-const closeBtn = langBox.querySelector('.header__close-btn');
-const workPopup = document.querySelector('.work-popup');
-const openWorkPopBtn = document.querySelector('.portfolio__add-btn');
-const closeWorkPopBtn = workPopup.querySelector('.work-popup__close-btn');
+const selectors = {
+  menu: '.header',
+  langBox: '.header__lang-box',
+  openBtn: '.presentation__open-menu',
+  closeBtn: '.header__close-btn',
+  workPopup: '.work-popup',
+  openWorkPopBtn: '.portfolio__add-btn',
+  closeWorkPopBtn: '.work-popup__close-btn',
+  workForm: '.work-popup__form',
+  workInputName: '.work-popup__input_name',
+  workInputImage: '.work-popup__input_image',
+  portfolioTitle: '.portfolio__title',
+  skillsTemplate: '.skills-template',
+  skillsPhoto: '.skills__photo',
+  skillsName: '.skills__program-name',
+  skillsContainer: '.skills__program',
+  skillsSection: '.skills__programs',
+  
+}
+
+const menu = document.querySelector(selectors.menu);
+const langBox = menu.querySelector(selectors.langBox);
+const openBtn = document.querySelector(selectors.openBtn);
+const closeBtn = langBox.querySelector(selectors.closeBtn);
+const workPopup = document.querySelector(selectors.workPopup);
+const openWorkPopBtn = document.querySelector(selectors.openWorkPopBtn);
+const closeWorkPopBtn = workPopup.querySelector(selectors.closeWorkPopBtn);
+const workForm = workPopup.querySelector(selectors.workForm);
+const workInputName = workForm.querySelector(selectors.workInputName);
+const workInputImage = workForm.querySelector(selectors.workInputImage);
+const portfolioTitle = document.querySelector(selectors.portfolioTitle);
 
 
 
-const openMenu = function() {
+function openMenu() {
   menu.classList.add('header_open');
   langBox.classList.add('header__lang-box_open');
 }
 
-const closeMenu = function() {
+function closeMenu() {
   menu.classList.remove('header_open');
   langBox.classList.remove('header__lang-box_open');
 }
 
-const openWorkPopup = function() {
+function openWorkPopup() {
+  workInputName.value = portfolioTitle.textContent;
   workPopup.classList.add('work-popup_open');
 }
 
-const closeWorkpopup = function() {
+function closeWorkpopup() {
   workPopup.classList.remove('work-popup_open');
 }
 
-const closeWorkByOverlay = function(evt) {
-  console.log(evt.target, evt.currentTarget);
+function closeWorkByOverlay(evt) {
   if(evt.target !== evt.currentTarget) {
     return
   };
   closeWorkpopup();
 }
 
+function handleFormSbm(evt) {
+  evt.preventDefault();
+  portfolioTitle.textContent = workInputName.value;
+}
+
+function createSkillsCard(name, image) {
+  const template = document.querySelector(selectors.skillsTemplate).content.querySelector(selectors.skillsContainer).cloneNode(true);
+  const skillsSection = document.querySelector(selectors.skillsSection);
+  const skillsPhoto = template.querySelector(selectors.skillsPhoto);
+  const skillsName = template.querySelector(selectors.skillsName);
+
+  skillsPhoto.src = image;
+  skillsPhoto.alt = name;
+  skillsName.textContent = name;
+
+  skillsSection.appendChild(template);
+}
+
+function createInitialSkillsCard() {
+  const initialSkills = [
+    {
+      name: 'Adobe Illustrator',
+      image: './images/AI.png',
+    },
+    {
+      name: 'Adobe After Effects',
+      image: './images/AE.png',
+    },
+    {
+      name: 'Adobe Photoshop',
+      image: './images/PS.png',
+    },
+    {
+      name: 'Figma',
+      image: './images/Figma.png',
+    },
+  ];
+
+  initialSkills.forEach(function(card) {
+    createSkillsCard(card.name, card.image);
+  });
+}
+
+createInitialSkillsCard();
+
+
 openBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
 openWorkPopBtn.addEventListener('click', openWorkPopup);
 closeWorkPopBtn.addEventListener('click', closeWorkpopup);
-workPopup.addEventListener('click', closeWorkByOverlay)
+workPopup.addEventListener('click', closeWorkByOverlay);
+workForm.addEventListener('submit', handleFormSbm);
